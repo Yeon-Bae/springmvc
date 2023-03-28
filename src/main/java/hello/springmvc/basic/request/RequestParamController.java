0b2c524311b06a52edb321b682679a9c27ad1bb9
세빈아빠집_인텔리제이 <sebinpapa@outlook.kr>
2023-03-28 23:48:05 +0900

    6. 스프링 MVC -  HTTP요청 파라미터 - @ModelAttribute

--- src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -1,7 +1,9 @@
 package hello.springmvc.basic.request;
 
+import hello.springmvc.basic.HelloData;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -46,7 +48,7 @@ public class RequestParamController {
 
     @ResponseBody
     @RequestMapping("/request-param-v4")
-    public String requestParamV4( String username, int age ) {
+    public String requestParamV4(String username, int age) {
         log.info("username={}, age={}", username, age);
         return "ok";
     }
@@ -56,7 +58,7 @@ public class RequestParamController {
     @RequestMapping("/request-param-required")
     public String requestParamRequired(
             @RequestParam(required = true) String username,
-            @RequestParam(required = false) Integer age ) {
+            @RequestParam(required = false) Integer age) {
         log.info("username={}, age={}", username, age);
         return "ok";
     }
@@ -66,7 +68,7 @@ public class RequestParamController {
     @RequestMapping("/request-param-default")
     public String requestParamDefault(
             @RequestParam(required = true, defaultValue = "sebin") String username,
-            @RequestParam(required = false, defaultValue = "5") Integer age ) {
+            @RequestParam(required = false, defaultValue = "5") Integer age) {
         log.info("username={}, age={}", username, age);
         return "ok";
     }
@@ -75,6 +77,24 @@ public class RequestParamController {
     @RequestMapping("/request-param-map")
     public String requestParamMap(@RequestParam Map<String, Object> paramMap) {
         log.info("username={}, age={}", paramMap.get("username"), paramMap.get("age"));
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("model-attribute-v1")
+    public String modelAttributeV1(@ModelAttribute HelloData helloData) {
+
+        log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
+
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("model-attribute-v2")
+    public String modelAttributeV2(HelloData helloData) {
+
+        log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
+
         return "ok";
     }
 
